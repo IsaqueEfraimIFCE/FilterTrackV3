1bb08974bf1bc1eefb3f10ea207dd9afc3cec540
IsaqueEfraimIFCE <isaque.efraim94@aluno.ifce.edu.br>
2026-04-23 09:39:42 -0300

    Includes

---
+++ app/src/main/java/com/example/filtertrack/WebAppInterface.kt
@@ -0,0 +1,34 @@
+package com.example.filtertrack
+
+import android.webkit.JavascriptInterface
+
+class WebAppInterface(
+    private val bleManager: BLEManager,
+    private val onAction: (Action) -> Unit,
+) {
+    sealed class Action {
+        object StartScan : Action()
+        object StopScan : Action()
+        data class Connect(val address: String) : Action()
+        object Disconnect : Action()
+        data class SendCommand(val cmd: String) : Action()
+    }
+
+    @JavascriptInterface
+    fun startScan() { onAction(Action.StartScan) }
+
+    @JavascriptInterface
+    fun stopScan() { onAction(Action.StopScan) }
+
+    @JavascriptInterface
+    fun connect(address: String) { onAction(Action.Connect(address)) }
+
+    @JavascriptInterface
+    fun disconnect() { onAction(Action.Disconnect) }
+
+    @JavascriptInterface
+    fun sendCommand(cmd: String) { onAction(Action.SendCommand(cmd)) }
+
+    @JavascriptInterface
+    fun isBluetoothEnabled(): Boolean = bleManager.isBluetoothEnabled()
+}
